9dfa5c3c32ff16f0e80d03a1eab3a6ea90b370e9
Anuar <orrenkek@gmail.com>
2021-02-26 20:44:38 +0500

    Lesson 11 - SocialWeb

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
+import React from 'react'
 import "./App.css";
-import Technologies from "./Technologies";
-import Header from "./Header";
+
+
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/768px-DC_Comics_logo.png" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://www.ontarioparks.com/images/headers/parks/winter/1200/longpoint.jpg" />
+        </div>
+        <div>ava + description</div>
+        <div>
+          My posts
+          <div>New Post</div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
     </div>
   );
 };
