bf2fb13699c5f8dcc36b28b05d68addd88b088d0
Anuar <sabirovanuar@gmail.com>
2021-02-27 14:50:46 +0500

    Lesson 14 - CSS modules

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import s from "./Header.module.css";
 
 const Header = () => {
     return(
-    <header className="header">
+    <header className={s.header}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/768px-DC_Comics_logo.png" />
     </header>
     )
--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import s from './ProfileContent.module.css'
 
 const ProfileContent = () => {
-    return(
-      <div className="content">
+    return (
+      <div className={s.content}>
         <div>
           <img src="https://www.ontarioparks.com/images/headers/parks/winter/1200/longpoint.jpg" />
         </div>
         <div>ava + description</div>
-          <div>
-            My posts
-            <div>New Post</div>
-            <div>post 1</div>
-            <div>post 2</div>
+        <div>
+          My posts
+          <div>New Post</div>
+          <div className={s.posts}>
+            <div className={s.item}>post 1</div>
+            <div className={s.item}>post 2</div>
           </div>
+        </div>
       </div>
-    )
+    );
 }
 
 export default ProfileContent
