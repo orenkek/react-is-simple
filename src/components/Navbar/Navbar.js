914a46036067ae91cc5d0bfee3efdca61c70daca
Anuar <sabirovanuar@gmail.com>
2021-02-28 14:36:53 +0500

    Lesson 20
    Chenged <a> to <NavLink>

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import classes from "./Navbar.module.css";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
-      <nav className={classes.nav}>
-        <div className={classes.item}>
-          <a href='/profile'>Profile</a>
-        </div>
-        <div className={classes.item}>
-          <a href='/dialogs'>Dialogs</a>
-        </div>
-        <div className={classes.item}>
-          <a href='/news'>News</a>
-        </div>
-        <div className={classes.item}>
-          <a href='/music'>Music</a>
-        </div>
-        <div className={classes.item}>
-          <a href='/settings'>Settings</a>
-        </div>
-      </nav>
+        <nav className={classes.nav}>
+            <div className={classes.item}>
+                <NavLink to='/profile'>Profile</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to='/dialogs'>Dialogs</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to='/news'>News</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to='/music'>Music</NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to='/settings'>Settings</NavLink>
+            </div>
+        </nav>
     );
 }
 
